582a7b38e63346fa57d6b806717deedcf3322753
Benoit Marsot <benoitmarsot@hotmail.com>
2025-03-19 22:51:49 -0700

    Remove old react directory.

--- cc-react/src/services/user.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const API_URL = "http://localhost:8080/api/users/";
-
-export const getUsers = async () => {
-    const response = await axios.get(API_URL+"users");
-    return response.data;
-};
-
