3880804f436c0d211a3e31008871d56c693b02c5
Tobias Tornqvist <tobias@snoweb.info>
2025-03-26 08:42:17 -0700

    remove debug banner from header

--- codechatui/lib/main.dart
+++ codechatui/lib/main.dart
@@ -51,6 +51,7 @@ class _MyAppState extends State<MyApp> {
       ],
       child: MaterialApp(
         title: 'Codechat',
+        debugShowCheckedModeBanner: false,
         theme: ThemeData(
           useMaterial3: true,
           brightness: Brightness.light,
